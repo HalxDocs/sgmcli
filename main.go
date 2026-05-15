ace903025d5e44189502f1263b9ea73aeee0e106
HalxDocs <kamsyejindu@gmail.com>
2026-05-15 11:11:21 +0100

    fix: add printReportCard function to display student report cards

--- main.go
+++ main.go
@@ -30,6 +30,23 @@ func letterGrade (avg float64) string {
 	}
 }
 
+func printReportCard(students []Student) {
+	fmt.Println("=============================")
+	fmt.Println("        REPORT CARD          ")
+	fmt.Println("=============================")
+
+
+for _, s := range students {
+	avg := calculateaverage(s.Score)
+	grade := letterGrade(avg)
+	fmt.Printf("Name: %s\n", s.Name)
+    fmt.Printf("Scores: %v\n", s.Score)
+    fmt.Printf("Average: %.2f\n", avg)
+	fmt.Printf("Grade: %s\n", grade)
+	fmt.Printf("-------------------------------")
+}
+}
+
 func main() {
      students := []Student{
           {
